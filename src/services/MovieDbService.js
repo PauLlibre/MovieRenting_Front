35155e6e82ea25208362a0ef17f47a434920b4c7
shuii18 <paullibre18@gmail.com>
2023-01-11 10:22:55 +0100

    Changed the api for the local api we createdwith our data

--- src/services/MovieDbService.js
+++ src/services/MovieDbService.js
@@ -6,10 +6,9 @@ export const MovieDbService = {};
 const apiKey = "cbd75f1c3e93985e760d1bdd211ec791";
 
 MovieDbService.getAllMovies = async () => {
-  let response = await axios.get(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
-  );
-  return response.data.results;
+  let response = await axios.get(`http://localhost:3000/movies`);
+  console.log(response);
+  return response.data.data;
 };
 
 MovieDbService.getImages = async (url) => {
