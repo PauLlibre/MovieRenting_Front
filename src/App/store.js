3467d308efd9321bc96c1f625e3b88bfccb20477
shuii18 <paullibre18@gmail.com>
2023-01-13 12:01:38 +0100

    Updates

--- src/App/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import loginReducer from "../features/login/loginSlice";
-import userDataReducer from "../features/userData/userDataSlice";
-
-export const store = configureStore({
-  reducer: loginReducer,
-});
--- src/Components/UserCard/UserCard.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./UserCard.scss";
-
-export default function UserCard({ user }) {
-  console.log(user.name);
-  return <div className="user">{user.name}</div>;
-}
